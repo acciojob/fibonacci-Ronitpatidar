d70c47a01cd5a93ea87f9a18a72a7294802be158
Ronitpatidar <ronitpatidar22@gmail.com>
2025-05-10 11:25:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function fibonacci(num) {
-// your code here
-	console.log(fibonacci(1)); 
-	console.log(fibonacci(5));
+function fibonacci(num) {
+    if (num === 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+
+    let a = 0, b = 1, c;
+    for (let i = 3; i <= num; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
 module.exports = fibonacci;
